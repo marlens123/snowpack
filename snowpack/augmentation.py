1624df335b6a3d14a05cf9d3483eb26cf7563fda
Jennifer Marks <j.faymarks@hotmail.com>
2024-11-26 16:19:17 -0500

    refactor augmentation module to use torchvision v2 and add normalization functions

--- snowpack/augmentation.py
+++ snowpack/augmentation.py
@@ -1,4 +1,6 @@
-from torchvision import transforms
+import numpy as np
+import torch
+from torchvision.transforms import v2
 
 
 def get_transformation(mean, std):
@@ -12,25 +14,59 @@ def get_transformation(mean, std):
     Returns:
         tuple: (train_transform, test_transform)
     """
-    train_transform = transforms.Compose(
+    train_transform = v2.Compose(
         [
-            transforms.ToTensor(),
+            normalize_image,
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Grayscale(3),  # Convert to 3-channel grayscale image
             # Crop a random portion of image and resize it to a given size.
-            transforms.RandomResizedCrop(size=1024, scale=(0.08, 1.0), ratio=(0.75, 1.33)),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5),
-            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
-            transforms.RandomRotation(degrees=15),
-            transforms.Normalize(mean=mean, std=std),
+            v2.RandomResizedCrop(size=1024, scale=(0.08, 1.0), ratio=(0.75, 1.33)),
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.RandomVerticalFlip(p=0.5),
+            v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+            v2.RandomRotation(degrees=15),  # type: ignore
+            v2.Normalize(mean=mean, std=std),
         ]
     )
 
-    test_transform = transforms.Compose(
+    test_transform = v2.Compose(
         [
-            transforms.ToTensor(),
-            transforms.Resize((1024, 1024)),
-            transforms.Normalize(mean=[mean], std=[std]),
+            normalize_image,
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Grayscale(3),
+            v2.Resize((1024, 1024)),
+            v2.Normalize(mean=mean, std=std),
         ]
     )
 
     return train_transform, test_transform
+
+
+def normalize_image(image: np.ndarray) -> np.ndarray:
+    """
+    Normalizes the pixel values of the given image to be between 0 and 1.
+
+    Args:
+        image (np.ndarray): The input image to normalize.
+
+    Returns:
+        np.ndarray: The normalized image with pixel values between 0 and 1.
+    """
+    min_val = image.min(axis=(0, 1), keepdims=True)
+    max_val = image.max(axis=(0, 1), keepdims=True)
+    normalized_image = (image - min_val) / (max_val - min_val)
+    return normalized_image
+
+
+def revert_normalization(image: torch.Tensor, mean, std):
+    """
+    This function reverts the normalization applied to the image.
+    Normalization is needed in the `transformation` pipeline and it ensure that the pixel values are between -1 and 1.
+    But for visualization, we need to revert the normalization to get the original pixel values, which are between 0 and 1.
+    """
+    mean = torch.tensor(mean).view(-1, 1, 1)
+    std = torch.tensor(std).view(-1, 1, 1)
+    reverted_image = image * std + mean
+    return reverted_image
