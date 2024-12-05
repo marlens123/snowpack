2437b879eecf1217fa1695a417ce0988d2a61d85
Olya-M <68296887+Olya-M@users.noreply.github.com>
2024-12-04 21:46:11 -0500

    ok maybe now

--- snowpack/dataset/data_utils.py
+++ snowpack/dataset/data_utils.py
@@ -9,10 +9,14 @@ def load_tifs_resize_to_np(image_path: str, mask_path: str, size: int = (1024, 1
     masks = []
 
     for _, i in enumerate(os.listdir(image_path)):
+        if '.DS_Store' in i:
+            continue
         image = rasterio.open(image_path + i).read(1)
         images.append(cv2.resize(image, size))
 
     for _, i in enumerate(os.listdir(mask_path)):
+        if '.DS_Store' in i:
+            continue
         mask= rasterio.open(mask_path + i).read(1)
         # use nearest neighbour interpolation to make sure that masks remain categorical
         masks.append(cv2.resize(mask, size, interpolation=cv2.INTER_NEAREST))
@@ -29,11 +33,15 @@ def load_tifs_resize_to_np_retain_ratio(image_path: str, mask_path: str, size: i
     masks = []
 
     for _, i in enumerate(os.listdir(image_path)):
+        if '.DS_Store' in i:
+            continue
         image = rasterio.open(image_path + i).read(1)
         r = np.min([size[0] / image.shape[1], size[0] / image.shape[0]])
         images.append(cv2.resize(image, (int(image.shape[1] * r), int(image.shape[0] * r))))
 
     for _, i in enumerate(os.listdir(mask_path)):
+        if '.DS_Store' in i:
+            continue
         mask= rasterio.open(mask_path + i).read(1)
         r = np.min([size[0] / mask.shape[1], size[0] / mask.shape[0]])
         # use nearest neighbour interpolation to make sure that masks remain categorical
